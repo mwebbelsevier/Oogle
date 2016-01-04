a9b743c30ce9c4a1bace1a67e73c681aedc27e85
Frieze, Ashley (ELS-OXF) <a.frieze@elsevier.com>
2016-01-04 09:55:08 +0000

    Latest version of the Oogle challenge - now in Java 7 and with increased quality of documentation.

---
+++ src/main/java/com/elsevier/OogleBasic.java
@@ -0,0 +1,28 @@
+package com.elsevier;
+
+import java.util.List;
+
+/**
+ * Implementation of the search engine without any consideration of performance
+ * Do the simplest implementation that can possibly work
+ * Do not worry about hardware or memory, but concern yourself with readability
+ * and getting the unit tests to pass
+ */
+public class OogleBasic implements Oogle {
+
+	public void add(Page page) {
+		// TODO 
+		
+	}
+
+	public List<Page> find(String... words) {
+		// TODO 
+		return null;
+	}
+
+	public int size() {
+		// TODO 
+		return -1;
+	}
+
+}
